93402a60387b99625af3c9e1593ec9c9566004f2
chloeji8888 <chloeji8888@gmail.com>
2024-08-24 16:16:31 -0700

    commented Icon@Mui UI

--- react-chat/src/App.js
+++ react-chat/src/App.js
@@ -1,3 +1,57 @@
+// import React from "react";
+// import {
+//   BrowserRouter as Router,
+//   Route,
+//   Routes,
+//   useNavigate,
+// } from "react-router-dom";
+// import Navbar from "./Navbar";
+// import Sidebar from "./Sidebar";
+// import ChatBox from "./ChatBox";
+// // import TaskPanel from "./TaskPanel";
+// import SaveExitPage from "./SaveExitPage";
+// import NewPage from "./NewPage";
+// import "./App.css";
+
+
+// const MainContent = () => {
+//   const navigate = useNavigate();
+
+//   const handleSaveExit = () => {
+//     navigate("/save-exit");
+//   };
+
+//   return (
+//     <div className="app">
+//       <Navbar />
+//       <div className="content">
+//         <Sidebar />
+//         <div className="main-content">
+//           <ChatBox />
+//         </div>
+//       </div>
+//       <footer className="footer">
+//         <button className="footer-button" onClick={handleSaveExit}>
+//           Save & Exit
+//         </button>
+//       </footer>
+//     </div>
+//   );
+// };
+
+// const App = () => {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<NewPage />} />
+//         <Route path="/main" element={<MainContent />} />
+//         <Route path="/save-exit" element={<SaveExitPage />} />
+//       </Routes>
+//     </Router>
+//   );
+// };
+
+// export default App;
 import React from "react";
 import {
   BrowserRouter as Router,
@@ -8,11 +62,11 @@ import {
 import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
 import ChatBox from "./ChatBox";
-// import TaskPanel from "./TaskPanel";
+import TaskPanel from "./TaskPanel";
 import SaveExitPage from "./SaveExitPage";
 import NewPage from "./NewPage";
+import AnalysisChart from "./chart";
 import "./App.css";
-
 
 const MainContent = () => {
   const navigate = useNavigate();
@@ -21,18 +75,28 @@ const MainContent = () => {
     navigate("/save-exit");
   };
 
+  const handleAnalysis = () =>{
+    navigate("/chart");
+  }
+
   return (
     <div className="app">
       <Navbar />
       <div className="content">
         <Sidebar />
         <div className="main-content">
-          <ChatBox />
+          <div className="content">
+            <TaskPanel />
+            <ChatBox />
+          </div>
         </div>
       </div>
       <footer className="footer">
         <button className="footer-button" onClick={handleSaveExit}>
           Save & Exit
+        </button>
+        <button className="footer-button" onClick={handleAnalysis}>
+          analysis
         </button>
       </footer>
     </div>
@@ -45,6 +109,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<NewPage />} />
         <Route path="/main" element={<MainContent />} />
+        <Route path="/chart" element={<AnalysisChart/>} />
         <Route path="/save-exit" element={<SaveExitPage />} />
       </Routes>
     </Router>
